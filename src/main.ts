96663fbdf1fd6b48384d46da299757411b97cc49
Reşat Yavçin <resatyavcin@icloud.com>
2025-12-14 00:05:58 +0300

    feat: connect db and init migration

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
